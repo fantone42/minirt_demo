db5906ba1efc52b565ac4207a5278ee94f28295a
Figg Antone <fantone@am-i3.msk.21-school.ru>
2021-04-07 19:07:44 +0300

    vvv

--- demonstration/srcs/intersections/sphere_intersect.c
+++ demonstration/srcs/intersections/sphere_intersect.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sphere_intersect.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fantone <fantone@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/07 15:21:28 by fantone           #+#    #+#             */
+/*   Updated: 2021/04/07 18:09:47 by fantone          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
 int     sphere_intersect(t_camera *cam,t_vector *ray, t_sphere *sphere)
@@ -19,7 +31,7 @@ int     sphere_intersect(t_camera *cam,t_vector *ray, t_sphere *sphere)
 	if (discr < 0)
 		return (0);
 	dist_1 = (b * (-1) - sqrt(discr))/2;
-	dist_2 = (b * (-1) + sqrt(discr))/2;
+	// dist_2 = (b * (-1) + sqrt(discr))/2;
 	if (dist_1 > 0)
 		return (1);
 	return (0);
